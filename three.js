389d1d8b555b41c17a1988e2d41771c7373272df
AnguloRecto <phoenixtracker305@gmail.com>
2024-05-28 08:24:48 -0400

    Update three.js

--- three.js
+++ three.js
@@ -1,4 +1,4 @@
-d document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function() {
     const scene = new THREE.Scene();
     const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
     const renderer = new THREE.WebGLRenderer();
